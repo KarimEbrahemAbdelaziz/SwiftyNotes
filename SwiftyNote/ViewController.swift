947897e4f402d096b0847fa95e645dcbcc7e1ff9
KEMansour <karim.ebrahem@areebgroup.com>
2019-07-06 12:24:07 +0200

    Init project

---
+++ SwiftyNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyNote
+//
+//  Created by Karim Ebrahem on 7/6/19.
+//  Copyright © 2019 Karim Ebrahem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
